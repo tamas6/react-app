59f76ff3c973c80706940bcf08feaa74e1ceddb6
tamas6 <81620050+tamas6@users.noreply.github.com>
2024-06-11 19:43:37 +0200

    chore: fix bugs :D

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,55 @@
+import { useState } from "react";
+import produce from "immer";
+function App() {
+	const [bugs, setBugs] = useState([
+		{ id: 1, title: "Bug 1", fixed: false },
+		{ id: 2, title: "Bug 2", fixed: false },
+	]);
+	const handleClick = () => {
+		// setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true }
+		setBugs(
+			produce((draft) => {
+				const bug = draft.find((bug) => bug.id === 1);
+				if (bug) bug.fixed = true;
+			})
+		);
+	};
+
+	return (
+		<div>
+			{bugs.map((bug) => (
+				<p key={bug.id}>
+					{bug.title} {bug.fixed ? "Fixed" : "New"}
+				</p>
+			))}
+			<button onClick={handleClick}>Click Me</button>
+		</div>
+	);
+}
+
+export default App;
+
+/*
+import { useState } from "react";
+import { Button } from "./components/Button/Button";
+function App() {
+	const [drink, setDrink] = useState({
+		title: "Americano",
+		price: 5,
+	});
+
+	const handleClick = () => {
+		setDrink({ ...drink, price: 6 });
+		console.log("clicked", `${drink.price}`);
+	};
+	return (
+		<div>
+			<button onClick={handleClick}>Click Me</button>
+		</div>
+	);
+}
+
+export default App;
 /// Managing component state
 
 import { useState } from "react";
@@ -15,7 +67,7 @@ export default App;
 
 // Practices
 
-/* import React, { useState } from "react";
+ import React, { useState } from "react";
 import { Button } from "./components/Button/Button";
 import styles from "./App.css";
 import Like from "./Like";
