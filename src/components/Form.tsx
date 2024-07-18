c2943c28c932ccabcedc0c57503a050ec73248d0
tamas6 <81620050+tamas6@users.noreply.github.com>
2024-07-18 17:13:15 +0200

    feat: required modes

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,21 @@
 import React, { FormEvent, useState } from "react";
+import { useForm, useFormState } from "react-hook-form";
 
+///  Controlled Components l51
+
+interface FormData {
+	name: string;
+	age: number;
+}
 const Form = () => {
-	const [person, setPerson] = useState({
-		name: "",
-		age: 0,
-	});
-	const handleSubmit = (event: FormEvent) => {
-		event.preventDefault();
-		console.log(person);
-	};
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>();
+
+	const onSubmit = (data: FieldValues) => console.log(data);
+
 	/*
 // Accessing input fields
 	const nameRef = useRef<HTMLInputElement>(null);
@@ -27,30 +34,30 @@ const Form = () => {
 	*/
 
 	return (
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit(onSubmit)}>
 			<div className="mb-3">
 				<label htmlFor="name" className="form-label">
 					Name
 				</label>
 				<input
-					onChange={(event) =>
-						setPerson({ ...person, name: event.target.value })
-					}
-					value={person.name}
+					{...register("name", { required: true, minLength: 3, maxLength: 6 })}
 					id="name"
 					type="text"
 					className="form-control"
 				/>
+				{errors.name?.type === "required" && (
+					<p className="text-danger">The namefield is required</p>
+				)}
+				{errors.name?.type === "minLength" && (
+					<p className="text-danger">The name must be at least 3 characters</p>
+				)}
 			</div>
 			<div className="mb-3">
 				<label htmlFor="age" className="form-label">
 					Age
 				</label>
 				<input
-					onChange={(event) =>
-						setPerson({ ...person, age: parseInt(event.target.value, 10) })
-					}
-					value={person.age}
+					{...register("age")}
 					id="age"
 					type="number"
 					className="form-control"
